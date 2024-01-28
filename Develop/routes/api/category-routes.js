63eafa654ac2f705ece39e87a88e3536f56ca11c
Tina Humphrey <humphrey14t@gmail.com>
2024-01-28 17:23:06 -0500

    commit changes made on catehory-routes.js file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,9 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((productData) => {
+    res.json(productData);
+  });
 });
 
 router.get('/:id', (req, res) => {
